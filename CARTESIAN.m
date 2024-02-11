e17b99a46c4bc284048026290f87df0ccfa3770d
Renzo M. Samson <157883261+Renzoooms@users.noreply.github.com>
2024-02-11 19:02:03 +0800

    Update CARTESIAN.m

--- CARTESIAN.m
+++ CARTESIAN.m
@@ -2,10 +2,10 @@ disp('CARTESIAN')
 syms a1 a2 a3 a4
 
 %% Link lengths
-a1 = 5;
-a2 = 5;
-a3 = 5;
-a4 = 5;
+a1 = 5
+a2 = 5
+a3 = 5
+a4 = 5
 
 %% D-H Parameters [theta, d, r, alpha, offset]
 % if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
