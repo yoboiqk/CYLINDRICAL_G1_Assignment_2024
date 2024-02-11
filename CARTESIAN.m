33403755fb3f9d5f95d65976011be0fcfcbd5cef
AlidoKevs <144081290+AlidoKevs@users.noreply.github.com>
2024-02-11 19:54:47 +0800

    CARTESIAN.m

--- CARTESIAN.m
+++ CARTESIAN.m
@@ -7,7 +7,7 @@ a2 = 5
 a3 = 5
 a4 = 5
 
-%% D-H Parameters [theta, d, r, alpha, offset]
+%% D-H parameters [theta, d, r, alpha, offset]
 % if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
 % if revolute joint: theta = 0,offset = 0, after offset put the value of theta
 
